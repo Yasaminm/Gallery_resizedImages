57fee2ec74130d3318a7d066f9005dc190e4958d
Yasamin Mustamandi <yasamin.mostamandy@gmail.com>
2018-03-22 10:53:32 +0100

    Upload images -function to upload one picture. -upload Images and resample to variety of sizes for thumbnail and big size -img files are tested according to their types -getimagesize() function is used to check the type. -function to upload multiple files. -name of input should be defined as array. -uniqueId() function is used to give uploaded files different name. -showing uploaded pictures in a grid system using bootstrap.

---
+++ includs/functions.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once './config.php';
+
+function calcDimension($x1,$y1,$x2){
+    $y2 = $y1 * $x2 / $x1;
+    return $y2;
+}
+function getRandName($prefix = ''){
+   
+    return str_replace('.','_',uniqid($prefix, true));
+}
+
+function getGdImage($path){
+   $info = getimagesize($path);
+   $img = false;
+   switch($info[2]){
+       case 1:
+           $img = imagecreatefromgif($path);
+        break;
+    case 2:
+           $img = imagecreatefromjpeg($path);
+        break;
+
+    case 3:
+           $img = imagecreatefrompng($path);
+        break;
+    
+    default:
+           $img = false;
+        break;
+   }
+    
+    return $img;
+    
+}
+
+function createResample($srcImg, $srcW, $srcH, $dstW, $dstH, $filetype, $path, $filename){
+$dstPath = false;
+$dstImg = imagecreatetruecolor($dstW, $dstH);
+imagecopyresampled($dstImg, $srcImg, 0, 0, 0, 0, $dstW, $dstH, $srcW, $srcH);
+
+if($filetype === 2){
+    $dstPath = $path. $filename. '.jpeg';
+    imagejpeg($dstImg, $dstPath);
+}elseif ($filetype === 3){
+    $dstPath = $path. $filename. '.png';
+    imagepng($dstImg, $dstPath);
+}else{
+    return false;
+}
+return $dstPath;
+}
---
+++ index.php
@@ -0,0 +1,60 @@
+
+<?php 
+//require means that afther loading required data the sile will be 
+//loaded otherwise it will kill the code to be loaded. it is more strong than include statement.
+require_once './config.php';
+require_once './includes/function.php';
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP 05 GALLERY</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <hr>
+        
+            
+        </div>
+        <pre>
+<?php
+//var_dump($srcImageInfo);
+?>
+        </pre>
+        
+    </body>
+</html>
